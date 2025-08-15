9e7eb1df855c8fd3007d30de44e76bd66ed6288e
Karan <karanmahatocse@gmail.com>
2025-08-16 00:03:22 +0530

    new post add

--- src/components/Blogs/FeaturedPosts.js
+++ src/components/Blogs/FeaturedPosts.js
@@ -10,14 +10,14 @@ const FeaturedPosts = ({ blogs }) => {
 
     <div className="grid grid-cols-2 grid-rows-2 gap-6  mt-10 sm:mt-16">
       <article className=" col-span-2  sxl:col-span-1 row-span-2 relative">
-        <BlogLayoutOne blog={sortedBlogs[0]} />
+        <BlogLayoutOne blog={sortedBlogs[1]} />
       </article>
       <article className=" col-span-2 sm:col-span-1 row-span-1 relative">
                 <BlogLayoutTwo blog={sortedBlogs[0]} />
 
       </article>
       <article className="col-span-2 sm:col-span-1 row-span-1 relative">
-                <BlogLayoutTwo blog={sortedBlogs[0]} />
+                <BlogLayoutTwo blog={sortedBlogs[1]} />
 
       </article>
     </div>
